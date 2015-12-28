c37c2398fd91b38dfca7da6293bbee6851a9366f
Alexandru Rosianu <rosian_alex@yahoo.com>
2015-12-28 13:53:59 +0200

    Adds overflow menu

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,14 @@
 import React, {Component, PropTypes} from 'react';
-import AppBar from 'material-ui/lib/app-bar';
 
 import CustomTheme from '../theme';
 import ThemeManager from 'material-ui/lib/styles/theme-manager';
 import ThemeDecorator from 'material-ui/lib/styles/theme-decorator';
+
+import AppBar from 'material-ui/lib/app-bar';
+import IconButton from 'material-ui/lib/icon-button';
+import IconMenu from 'material-ui/lib/menus/icon-menu';
+import MoreVertIcon from 'material-ui/lib/svg-icons/navigation/more-vert';
+import MenuItem from 'material-ui/lib/menus/menu-item';
 
 @ThemeDecorator(ThemeManager.getMuiTheme(CustomTheme))
 class App extends Component {
@@ -14,7 +19,22 @@ class App extends Component {
   render() {
     return (
       <div>
-        <AppBar title="Tarkan's Awesome Chat App" />
+        <AppBar
+          title="Tarkan's Awesome Chat App"
+          iconElementRight={
+            <IconMenu
+              iconButtonElement={
+                <IconButton><MoreVertIcon /></IconButton>
+              }
+              targetOrigin={{horizontal: 'right', vertical: 'top'}}
+              anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+            >
+              <MenuItem primaryText="Refresh" />
+              <MenuItem primaryText="Help" />
+              <MenuItem primaryText="Sign out" />
+            </IconMenu>
+          }
+        />
         {this.props.children}
       </div>
     );
